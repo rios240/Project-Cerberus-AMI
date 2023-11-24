f427a590645d8a620422be87779577dc0a55e762
Tyler <tylerios@colostate.edu>
2023-11-24 14:09:37 -0700

    change state_info state types

--- core/pldm_firmware/pldm_firmware_update_fsm.h
+++ core/pldm_firmware/pldm_firmware_update_fsm.h
@@ -5,33 +5,24 @@
 
 #include <stdint.h>
 #include "base.h"
+#include "firmware_update.h"
 #include "cmd_interface/cmd_interface.h"
 
 #define MAX_ROW 1
 
-enum pldm_firmware_device_states {
-	PLDM_FD_STATE_IDLE = 0,
-	PLDM_FD_STATE_LEARN_COMPONENTS = 1,
-	PLDM_FD_STATE_READY_XFER = 2,
-	PLDM_FD_STATE_DOWNLOAD = 3,
-	PLDM_FD_STATE_VERIFY = 4,
-	PLDM_FD_STATE_APPLY = 5,
-	PLDM_FD_STATE_ACTIVATE = 6
-};
-
 struct pldm_firmware_device_state_info {
     int (*pldm_command_controller)(struct pldm_firmware_device_state_info *state_info, 
-        struct cmd_interface_msg *message, int command);
+        struct cmd_interface_msg *message);
 
-    int state;
-    int update_mode;
+    enum pldm_firmware_device_states state;
+    bool update_mode;
     int response;
-    int next_state;
+    enum pldm_firmware_device_states next_state;
 };
 
 struct pldm_firmware_device_fsm {
     enum pldm_firmware_device_states current_state;
-    struct pldm_firmware_device_state_info sst[MAX_ROW];
+    struct pldm_firmware_device_state_info stt[MAX_ROW];
 };
 
 
