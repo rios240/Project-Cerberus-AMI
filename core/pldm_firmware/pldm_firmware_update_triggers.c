841cbaac2fe56ac34d51a1b6f8cb5f8528609ee1
Tyler <tylerios@colostate.edu>
2023-11-24 14:10:19 -0700

    update

--- core/pldm_firmware/pldm_firmware_update_triggers.c
+++ core/pldm_firmware/pldm_firmware_update_triggers.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include "pldm_firmware_update_triggers.h"
+#include "pldm_firmware_update_fsm.h"
 #include "utils.h"
 #include "firmware_update.h"
 
@@ -11,13 +12,9 @@
 #define METADATA false
 
 
-int requestUpdate(struct pldm_msg *pldmMsg, struct cmd_interface_msg *message, 
-                    int *completion_code, int command, int current_state, int update_mode) 
+int requestUpdate(struct pldm_firmware_device_state_info *state_info, struct pldm_msg *pldmMsg, struct cmd_interface_msg *message) 
 {
 
-    if (command != PLDM_REQUEST_UPDATE) {
-        return command;
-    }
 
     uint32_t max_transfer_size = 0;
     uint16_t num_of_comp = 0;
@@ -27,17 +24,12 @@ int requestUpdate(struct pldm_msg *pldmMsg, struct cmd_interface_msg *message,
     uint8_t comp_image_set_ver_str_len = 0;
     struct variable_field comp_img_set_ver_str = { 0 };
 
-    int status = decode_request_update_req(pldmMsg, sizeof(struct pldm_request_update_req) + sizeof (struct variable_field), 
+    uint8_t completion_code = decode_request_update_req(pldmMsg, sizeof(struct pldm_request_update_req) + sizeof (struct variable_field), 
                                 &max_transfer_size, &num_of_comp, &max_outstanding_transfer_req,
                                 &pkg_data_len, &comp_image_set_ver_str_type, &comp_image_set_ver_str_len,
                                 &comp_img_set_ver_str);
 
-    if (status != PLDM_SUCCESS) {
-        return status;
-    }
-
-    uint8_t completion_code = 0;
-    if (current_state == READY_XFER && update_mode) {
+    if (state_info->state == PLDM_FD_STATE_READY_XFER && state_info->update_mode) {
         completion_code = PLDM_FWUP_ALREADY_IN_UPDATE_MODE;
     }
     else if (RETRY) {
@@ -61,12 +53,14 @@ int requestUpdate(struct pldm_msg *pldmMsg, struct cmd_interface_msg *message,
     respBuf[0] = MCTP_BASE_PROTOCOL_MSG_TYPE_PLDM;
     struct pldm_msg *respMsg = (struct pldm_msg *)&respBuf[1];
 
-    status = encode_request_update_resp(instance_id, fd_meta_data_len, 
+    int status = encode_request_update_resp(instance_id, fd_meta_data_len, 
                                 fd_will_send_pkg_data, completion_code, respMsg, sizeof(struct pldm_request_update_resp));
     
     memcpy(message->data, respBuf, sizeof (respBuf));
     message->length = sizeof (respBuf);
 
+    state_info->response = completion_code;
+    
     return status;
     
 
--- core/pldm_firmware/pldm_firmware_update_triggers.h
+++ core/pldm_firmware/pldm_firmware_update_triggers.h
@@ -6,8 +6,7 @@
 #include "pldm_firmware_update_fsm.h"
 
 
-int requestUpdate(struct pldm_msg *pldmMsg, struct cmd_interface_msg *message, 
-                    int *completion_code, int command, int current_state, int update_mode);
+int requestUpdate(struct pldm_firmware_device_state_info *state_info, struct pldm_msg *pldmMsg, struct cmd_interface_msg *message);
 
 
 #endif /* PLDM_FIRMWARE_UPDATE_TRIGGERS_H_ */
