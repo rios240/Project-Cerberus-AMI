36b1dbf42189c51ae7858c4b3229a4483e5ebe86
Tyler <tylerios@colostate.edu>
2024-03-24 18:45:09 -0600

    Created cmd_interface for pldm_fwup header file, deleted depreicated cmd_channel

---
+++ core/pldm_fwup/cmd_interface_pldm_fwup.h
@@ -0,0 +1,22 @@
+#ifndef CMD_INTERFACE_PLDM_FWUP_H_
+#define CMD_INTERFACE_PLDM_FWUP_H_
+
+
+#include <stdint.h>
+#include "platform_config.h"
+#include "common/observable.h"
+#include "cmd_interface/cmd_interface.h"
+
+/**
+ * Command interface for processing received PLDM FWUP protocol requests.
+ */
+struct cmd_interface_pldm_fwup {
+    struct cmd_interface base;                      /**< Base command interface */     
+};
+
+
+int cmd_interface_mctp_control_init (struct cmd_interface_pldm_fwup *intf);
+void cmd_interface_mctp_control_deinit (struct cmd_interface_pldm_fwup *intf);
+
+
+#endif /* CMD_INTERFACE_PLDM_FWUP_H_ */
