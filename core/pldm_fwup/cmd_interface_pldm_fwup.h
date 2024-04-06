e72fb6d12cbecafc5b2abd44a67e1b8505536b82
Tyler <tylerios@colostate.edu>
2024-04-05 20:24:24 -0600

    pldm_fwup: added UA and FD directives to cmd_interface_pldm_fwup

--- core/pldm_fwup/cmd_interface_pldm_fwup.h
+++ core/pldm_fwup/cmd_interface_pldm_fwup.h
@@ -13,31 +13,38 @@
  * The flash addresses and devices to use for different PLDM FWUP regions.
  */
 struct pldm_fwup_flash_map {
-    const struct flash_virtual_disk *package_data_flash;        /**< The flash device that contains the PackaheData. */
-    uint32_t package_data_addr;                                 /**< The base address of the PackageData region. */
-    size_t package_data_size;                                   /**< The size of the PackageData region. */
-    const struct flash_virtual_disk *device_meta_data_flash;    /**< The flash device that contains the DeviceMetaData. */
-    uint32_t device_meta_data_addr;                             /**< The base address of the DeviceMetaData region. */
-    size_t device_meta_data_size;                               /**< The size of the DeviceMetaData region. */
+#ifdef PLDM_FWUP_FD_ENABLE
+    const struct flash_virtual_disk *package_data;                      /**< The flash device that contains the PackageData. */
+    uint32_t package_data_addr;                                         /**< The base address of the PackageData region. */
+    size_t package_data_size;                                           /**< The size of the PackageData region. */
+#elif PLDM_FWUP_UA_ENABLE
+    const struct flash_virtual_disk *device_meta_data;                  /**< The flash device that contains the DeviceMetaData. */
+    uint32_t device_meta_data_addr;                                     /**< The base address of the DeviceMetaData region. */
+    size_t device_meta_data_size;                                       /**< The size of the DeviceMetaData region. */
+    const struct flash_virtual_disk *firmware_update_package;           /**< The flash device that contains the Firmware Update Package. */
+    uint32_t firmware_update_package_addr;                              /**< The base address of the Firmware Update Package region. */
+    size_t firmware_update_package_size;                                /**< The size of the Firmware Update Package region. */
+#endif
 };
 
+
 /**
- * Multipart transfer context
+ * Multipart transfer context for PLDM Get commands
 */
 struct pldm_fwup_multipart_transfer_handler {
 #ifdef PLDM_FWUP_FD_ENABLE
-    uint32_t transfer_handle;                                   /**< Handle that is used to identify a package data transfer. */
-    uint8_t transfer_op_flag;                                   /**< Operation flag that indiates whether this is the start of the transfer. */
+    uint32_t transfer_handle;                                           /**< Handle that is used to identify a package data transfer. */
+    uint8_t transfer_op_flag;                                           /**< Operation flag that indiates whether this is the start of the transfer. */
 #elif PLDM_FWUP_UA_ENABLE
-    uint32_t next_transfer_handle;                              /**< Handle that is used to identify the next portion of the transfer. */
-    uint8_t transfer_flag;                                      /**< Transfer flag that indiates what part of the transfer this response represents. */
+    uint32_t next_transfer_handle;                                      /**< Handle that is used to identify the next portion of the transfer. */
+    uint8_t transfer_flag;                                              /**< Transfer flag that indiates what part of the transfer this response represents. */
 #endif
 };
 
 /**
  * Variable context for a cmd interface PLDM FWUP.
  */
-struct cmd_interface_pldm_fwup_state {
+struct pldm_fwup_state {
     uint8_t state;                                                      /**< Current PLDM FWUP state. */
     uint8_t command;                                                    /**< Last PLDM FWUP command. */
     uint8_t completion_code;                                            /**< Last PLDM completion code. */
@@ -48,11 +55,11 @@ struct cmd_interface_pldm_fwup_state {
  */
 struct cmd_interface_pldm_fwup {
     struct cmd_interface base;                                          /**< Base command interface */
-    struct pldm_fwup_multipart_transfer_handler *multipart_transfer;    /**< Handler for multipart transfer for Get commands. */    
+    struct pldm_fwup_multipart_transfer_handler multipart_transfer;    /**< Handler for multipart transfer for Get commands. */    
 	const struct pldm_fwup_flash_map *flash_map;                        /**< The flash address mapping to use for PLDM FWUP. */
     const struct firmware_update_control *control;		                /**< FW update control instance */
 #ifdef PLDM_FWUP_FD_ENABLE
-    struct cmd_interface_pldm_fwup_state *state;                        /**< Variable context for the cmd interface instance. */
+    struct pldm_fwup_state *state;                                      /**< Variable context for the cmd interface instance. */
 #endif
 
 
@@ -71,7 +78,7 @@ struct cmd_interface_pldm_fwup {
 
 
 int cmd_interface_pldm_fwup_init (struct cmd_interface_pldm_fwup *intf, struct pldm_fwup_flash_map *flash_map,
-    const struct cmd_interface_pldm_fwup_state *state_ptr, const struct firmware_update_control *control, uint8_t init_state);
+    struct pldm_fwup_state *state_ptr, const struct firmware_update_control *control, uint8_t init_state);
 void cmd_interface_pldm_fwup_deinit (struct cmd_interface_pldm_fwup *intf);
 
 
