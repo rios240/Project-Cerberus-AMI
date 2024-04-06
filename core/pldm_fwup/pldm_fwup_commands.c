a422d7d3c60e1ea15bd3fd5f5c0aa39f225f5795
Tyler <tylerios@colostate.edu>
2024-04-05 20:26:39 -0600

    pldm_fwup: commented out pldm_fwup_generate_get_package_data_request

--- core/pldm_fwup/pldm_fwup_commands.c
+++ core/pldm_fwup/pldm_fwup_commands.c
@@ -1,15 +1,15 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdio.h>
-#include "utils.h"
+#include "libpldm/utils.h"
 #include "pldm_fwup_commands.h"
-#include "firmware_update.h"
+#include "libpldm/firmware_update.h"
 
-
+/*
 int pldm_fwup_generate_get_package_data_request(struct pldm_fwup_flash_map *flash_map, uint8_t *buffer, size_t buf_len)
 {
     struct pldm_multipart_transfer_req req_data;
     
 
 }
-
+*/
