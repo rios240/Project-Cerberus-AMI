24122fdc40b535d9d90c78bb77b91e4345146c32
Tyler <tylerios@colostate.edu>
2024-04-08 10:24:06 -0600

    pldm_fwup: add directives for includes

--- core/pldm_fwup/pldm_fwup_commands.c
+++ core/pldm_fwup/pldm_fwup_commands.c
@@ -1,9 +1,16 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdio.h>
-#include "libpldm/utils.h"
 #include "pldm_fwup_commands.h"
+
+#ifdef PLDM_FWUP_FD_ENABLE
 #include "libpldm/firmware_update.h"
+#include "libpldm/utils.h"
+#elif PLDM_FWUP_UA_ENABLE
+#include "firmware_update.h"
+#include "utils.h"
+#endif
+
 
 /*
 int pldm_fwup_generate_get_package_data_request(struct pldm_fwup_flash_map *flash_map, uint8_t *buffer, size_t buf_len)
