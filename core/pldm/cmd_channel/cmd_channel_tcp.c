a58a97d45dbbff10d15e9372ceb49d3b7c0b16b0
Tyler <tylerios@colostate.edu>
2024-05-18 20:01:20 -0600

    pldm/cmd_channel: send and receive

--- core/pldm/cmd_channel/cmd_channel_tcp.c
+++ core/pldm/cmd_channel/cmd_channel_tcp.c
@@ -21,13 +21,13 @@ void print_packet_data(const uint8_t *data, size_t len) {
     printf("\n");
 }
 
-int set_socket_non_blocking(int socket_fd) {
-    int flags = fcntl(socket_fd, F_GETFL, 0);
-    if (flags == -1) return -1;
-    return fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK);
+int set_socket_non_blocking(int sockfd) {
+    int flags = fcntl(sockfd, F_GETFL, 0);
+    if (flags == -1) {
+        return -1;
+    }
+    return fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
 }
-
-
 
 /**
 * Receive a command packet from a communication channel.  This call will block until a packet
@@ -39,7 +39,7 @@ int set_socket_non_blocking(int socket_fd) {
 * negative value will wait forever, and a value of 0 will return immediately.
 *
 * @return 0 if a packet was successfully received or an error code.
-*/
+*
 int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int ms_timeout) {
     int server_fd, new_socket;
     struct sockaddr_in address;
@@ -117,6 +117,7 @@ int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int m
 
     return 0;
 }
+*/
 
 
 /**
@@ -130,7 +131,7 @@ int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int m
 * @param packet The packet to send.
 *
 * @return 0 if the the packet was successfully sent or an error code.
-*/
+*
 int send_packet(struct cmd_channel *channel, struct cmd_packet *packet) {
     struct sockaddr_in serv_addr;
     int sock = 0;
@@ -179,6 +180,153 @@ int send_packet(struct cmd_channel *channel, struct cmd_packet *packet) {
 
     //printf("Sent a packet.\n");
     //print_packet_data(packet->data, packet->pkt_size);
+    close(sock);
+
+    return 0;
+}
+*/
+
+
+/**
+* Receive a command packet from a communication channel.  This call will block until a packet
+* has been received or the timeout has expired.
+*
+* @param channel The channel to receive a packet from.
+* @param packet Output for the packet data being received.
+* @param ms_timeout The amount of time to wait for a received packet, in milliseconds.  A
+* negative value will wait forever, and a value of 0 will return immediately.
+*
+* @return 0 if a packet was successfully received or an error code.
+*/
+int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int ms_timeout) {
+    static int server_fd = -1;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    if (server_fd == -1) {
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+            return CMD_CHANNEL_TCP_ERROR;
+        }
+
+        if (set_socket_non_blocking(server_fd) < 0) {
+            close(server_fd);
+            return CMD_CHANNEL_TCP_ERROR;
+        }
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+            close(server_fd);
+            return CMD_CHANNEL_TCP_ERROR;
+        }
+
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons(PORT);
+
+        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+            close(server_fd);
+            return CMD_CHANNEL_TCP_ERROR;
+        }
+        if (listen(server_fd, 3) < 0) {
+            close(server_fd);
+            return CMD_CHANNEL_TCP_ERROR;
+        }
+    }
+
+    struct timeval start, now, timeout;
+    gettimeofday(&start, NULL);
+    long elapsed_ms = 0;
+
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(server_fd, &readfds);
+
+    timeout.tv_sec = ms_timeout / 1000;
+    timeout.tv_usec = (ms_timeout % 1000) * 1000;
+
+    int new_socket = -1;
+    while (elapsed_ms < ms_timeout) {
+        fd_set tmp_fds = readfds;
+        int activity = select(server_fd + 1, &tmp_fds, NULL, NULL, &timeout);
+
+        if (activity < 0 && errno != EINTR) {
+            return CMD_CHANNEL_TCP_ERROR;
+        } else if (activity > 0 && FD_ISSET(server_fd, &tmp_fds)) {
+            new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+            if (new_socket >= 0) {
+                break;
+            }
+        }
+
+        gettimeofday(&now, NULL);
+        elapsed_ms = (now.tv_sec - start.tv_sec) * 1000 + (now.tv_usec - start.tv_usec) / 1000;
+    }
+
+    if (new_socket < 0) {
+        return CMD_CHANNEL_TCP_ERROR;
+    }
+
+    size_t valread = read(new_socket, packet->data, MCTP_BASE_PROTOCOL_MAX_PACKET_LEN);
+
+    packet->pkt_size = valread;
+    packet->dest_addr = (uint8_t)cmd_channel_get_id(channel);
+
+    close(new_socket);
+
+    return 0;
+}
+
+
+
+/**
+* Send a command packet over a communication channel.
+*
+* Returning from this function does not guarantee the packet has been fully transmitted.
+* Depending on the channel implementation, it is possible the packet is still in flight with
+* the data buffered in the channel driver.
+*
+* @param channel The channel to send a packet on.
+* @param packet The packet to send.
+*
+* @return 0 if the the packet was successfully sent or an error code.
+*/
+int send_packet(struct cmd_channel *channel, struct cmd_packet *packet) {
+    struct sockaddr_in serv_addr;
+    int sock = 0;
+    int ms_timeout = 60000;
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        return CMD_CHANNEL_TCP_ERROR;
+    }
+
+    struct timeval start, now;
+    gettimeofday(&start, NULL);
+    long elapsed_ms = 0;
+    int connected = 0;
+
+    while (elapsed_ms < ms_timeout && !connected) {
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            return CMD_CHANNEL_TCP_ERROR;
+        }
+
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) >= 0) {
+            connected = 1;
+        } else {
+            close(sock);
+        }
+
+        gettimeofday(&now, NULL);
+        elapsed_ms = (now.tv_sec - start.tv_sec) * 1000 + (now.tv_usec - start.tv_usec) / 1000;
+    }
+
+    if (!connected) {
+        return CMD_CHANNEL_TCP_ERROR;
+    }
+
+    send(sock, packet->data, packet->pkt_size, 0);
     close(sock);
 
     return 0;
