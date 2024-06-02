1d8669a220a396d4ed637d646dde4eb0d76285b6
Tyler <tylerios@colostate.edu>
2024-06-02 17:13:54 -0600

    pldm/cmd_channel: switch back to TCP

--- core/pldm/cmd_channel/cmd_channel_tcp.c
+++ core/pldm/cmd_channel/cmd_channel_tcp.c
@@ -23,6 +23,7 @@
 #include <stdint.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include <fcntl.h>
 #include "cmd_channel_tcp.h"
 #include "platform_io.h"
 
@@ -35,15 +36,15 @@ void print_packet_data(const uint8_t *data, size_t len) {
     printf("\n");
 }
 
-/*
+
 int set_socket_non_blocking(int sockfd) {
     int flags = fcntl(sockfd, F_GETFL, 0);
     if (flags == -1) {
-        return CMD_CHANNEL_TCP_ERROR;
+        return CMD_CHANNEL_SOCKET_ERROR;
     }
     return fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
 }
-*/
+
 
 /**
 * Receive a command packet from a communication channel.  This call will block until a packet
@@ -55,7 +56,7 @@ int set_socket_non_blocking(int sockfd) {
 * negative value will wait forever, and a value of 0 will return immediately.
 *
 * @return 0 if a packet was successfully received or an error code.
-*
+*/
 int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int ms_timeout) {
     int server_fd, new_socket;
     struct sockaddr_in address;
@@ -64,31 +65,31 @@ int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int m
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        return CMD_CHANNEL_TCP_ERROR;
+        return CMD_CHANNEL_SOCKET_ERROR;
     }
 
     if (set_socket_non_blocking(server_fd) < 0) {
         close(server_fd);
-        return CMD_CHANNEL_TCP_ERROR;
+        return CMD_CHANNEL_SOCKET_ERROR;
     }
     
     // Forcefully attaching socket to the port 5000
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        return CMD_CHANNEL_TCP_ERROR;
+        return CMD_CHANNEL_SOCKET_ERROR;
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(CMD_CHANNEL_PORT);
 
 
     // Bind the socket to the address
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        return CMD_CHANNEL_TCP_ERROR;
+        return CMD_CHANNEL_SOCKET_ERROR;
     }
 
     // Listen for incoming connections
     if (listen(server_fd, 3) < 0) {
-        return CMD_CHANNEL_TCP_ERROR;
+        return CMD_CHANNEL_SOCKET_ERROR;
     }
 
     struct timeval start, now;
@@ -103,7 +104,7 @@ int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int m
 
     if (new_socket < 0) {
         close(server_fd);
-        return CMD_CHANNEL_TCP_ERROR;
+        return CMD_CHANNEL_SOCKET_ERROR;
     }
     
     //platform_mutex_lock(&channel->lock);
@@ -119,7 +120,7 @@ int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int m
     close(server_fd);
 
     return 0;
-}*/
+}
 
 
 
@@ -134,7 +135,7 @@ int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int m
 * @param packet The packet to send.
 *
 * @return 0 if the the packet was successfully sent or an error code.
-*
+*/
 int send_packet(struct cmd_channel *channel, struct cmd_packet *packet) {
     struct sockaddr_in serv_addr;
     int sock = 0;
@@ -142,10 +143,10 @@ int send_packet(struct cmd_channel *channel, struct cmd_packet *packet) {
 
     memset(&serv_addr, '0', sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(CMD_CHANNEL_PORT);
 
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        return CMD_CHANNEL_TCP_ERROR;
+        return CMD_CHANNEL_SOCKET_ERROR;
     }
 
     struct timeval start, now;
@@ -155,7 +156,7 @@ int send_packet(struct cmd_channel *channel, struct cmd_packet *packet) {
 
     while (elapsed_ms < ms_timeout && !connected) {
         if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-            return CMD_CHANNEL_TCP_ERROR;
+            return CMD_CHANNEL_SOCKET_ERROR;
         }
 
         if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) >= 0) {
@@ -169,7 +170,7 @@ int send_packet(struct cmd_channel *channel, struct cmd_packet *packet) {
     }
 
     if (!connected) {
-        return CMD_CHANNEL_TCP_ERROR;
+        return CMD_CHANNEL_SOCKET_ERROR;
     }
 
     //platform_mutex_lock(&channel->lock);
@@ -181,113 +182,4 @@ int send_packet(struct cmd_channel *channel, struct cmd_packet *packet) {
     close(sock);
 
     return 0;
-}*/
-
-int setup_server_socket() {
-    static pthread_mutex_t remove_socket_mutex = PTHREAD_MUTEX_INITIALIZER;
-    static bool socket_removed = false;
-
-    int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        platform_printf("socket"NEWLINE);
-        return CMD_CHANNEL_SOCKET_ERROR;
-    }
-
-    struct sockaddr_un addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, CMD_CHANNEL_SOCKET_PATH, sizeof(addr.sun_path) - 1);
-
-    pthread_mutex_lock(&remove_socket_mutex);
-
-    if (access(CMD_CHANNEL_SOCKET_PATH, F_OK) == 0) {
-        pthread_mutex_lock(&remove_socket_mutex);
-        if (!socket_removed) {
-            platform_printf("Removing existing socket file" NEWLINE);
-            if (unlink(CMD_CHANNEL_SOCKET_PATH) < 0) {
-                platform_printf("unlink" NEWLINE);
-                close(sockfd);
-                pthread_mutex_unlock(&remove_socket_mutex);
-                return CMD_CHANNEL_SOCKET_ERROR;
-            }
-            socket_removed = true;
-        }
-        pthread_mutex_unlock(&remove_socket_mutex);
-    }
-
-    pthread_mutex_unlock(&remove_socket_mutex);
-
-    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        platform_printf("bind"NEWLINE);
-        close(sockfd);
-        return CMD_CHANNEL_SOCKET_ERROR;
-    }
-
-    if (listen(sockfd, 5) < 0) {
-        platform_printf("listen"NEWLINE);
-        close(sockfd);
-        return CMD_CHANNEL_SOCKET_ERROR;
-    }
-
-    return sockfd;
-}
-
-int receive_packet(struct cmd_channel *channel, struct cmd_packet *packet, int ms_timeout) {
-    int sockfd = setup_server_socket();
-    if (sockfd < 0) {
-        return sockfd;
-    }
-
-    int connfd = accept(sockfd, NULL, NULL);
-    if (connfd < 0) {
-        platform_printf("accept"NEWLINE);
-        close(sockfd);
-        return CMD_CHANNEL_SOCKET_ERROR;
-    }
-
-    struct timeval timeout;
-    timeout.tv_sec = ms_timeout / 1000;
-    timeout.tv_usec = (ms_timeout % 1000) * 1000;
-
-    setsockopt(connfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-
-    ssize_t received = recv(connfd, packet->data, CMD_MAX_PACKET_SIZE, 0);
-
-    packet->pkt_size = received;
-    packet->dest_addr = (uint8_t)cmd_channel_get_id(channel);
-
-    close(connfd);
-    close(sockfd);
-    return (size_t) received == packet->pkt_size && received != -1 ? 0 : CMD_CHANNEL_SOCKET_ERROR;
-}
-
-int send_packet(struct cmd_channel *channel, struct cmd_packet *packet) {
-    int sockfd;
-    struct sockaddr_un addr;
-
-    memset(&addr, 0, sizeof(addr));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, CMD_CHANNEL_SOCKET_PATH, sizeof(addr.sun_path) - 1);
-
-    while (1) {
-        sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (sockfd < 0) {
-            platform_printf("socket"NEWLINE);
-            return CMD_CHANNEL_SOCKET_ERROR;
-        }
-
-        if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-            close(sockfd);
-            sleep(1);
-            continue;
-        }
-
-        break;
-    }
-
-    ssize_t sent = send(sockfd, packet->data, packet->pkt_size, 0);
-
-    close(sockfd);
-
-    return (size_t) sent == packet->pkt_size && sent != -1 ? 0 : CMD_CHANNEL_SOCKET_ERROR;
 }
