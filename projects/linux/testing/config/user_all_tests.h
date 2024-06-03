5da89d0dc3ecec86f9ecbd10119bb510f2ee4b43
Tyler <tylerios@colostate.edu>
2024-06-02 21:40:26 -0600

    projects/linux: removed user all tests from gitignore

---
+++ projects/linux/testing/config/user_all_tests.h
@@ -0,0 +1,15 @@
+#ifndef USER_ALL_TESTS_H_
+#define USER_ALL_TESTS_H_
+
+/** Testing configuration for AMI PLDM Firmware Update project. 
+ * 
+ * Skips all but the PLDM Firmware Update tests.
+ * 
+*/
+
+#define TESTING_SKIP_ALL_TESTS
+
+#define TESTING_RUN_PLDM_FWUP_TRANSFERS_SUITE
+
+
+#endif /* USER_ALL_TESTS_H_ */
