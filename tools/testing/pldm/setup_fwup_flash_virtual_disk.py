69f9c8e4671464a50c971aa71578d904ff3633f2
Tyler <tylerios@colostate.edu>
2024-06-03 02:00:54 -0600

    tools: added virtual flash setup for pldm fwup testing

---
+++ tools/testing/pldm/setup_fwup_flash_virtual_disk.py
@@ -0,0 +1,21 @@
+import os
+import random
+
+'''Virtual flash setup for PLDM FWUP testing.'''
+
+def setup_fwup_flash_virtual_disk(filename, regions):
+    with open(filename, 'wb') as f:
+        for region in regions:
+            if region == 'empty':
+                f.write(bytes(1024 * 1024))
+            elif region == 'random':
+                f.write(os.urandom(1024 * 1024)) 
+
+
+fd_flash_regions = ['empty', 'random', 'empty', 'empty']
+ua_flash_regions = ['random', 'empty', 'random', 'random']
+
+setup_fwup_flash_virtual_disk('../../../core/testing/pldm/flash_virtual_disk/fd_flash.bin', fd_flash_regions)
+setup_fwup_flash_virtual_disk('../../../core/testing/pldm/flash_virtual_disk/ua_flash.bin', ua_flash_regions)
+
+print("Flash virtual disk setup successfully.")
