a9167b72b6b691cb9ed535f9673eefb0f04c2aae
Tyler <tylerios@colostate.edu>
2024-06-04 19:04:48 -0600

    testingL PLDM FWUP switched order of tests

--- core/testing/pldm/fwup_fd/pldm_fwup_protocol_fd_commands_test.c
+++ core/testing/pldm/fwup_fd/pldm_fwup_protocol_fd_commands_test.c
@@ -281,8 +281,8 @@ TEST_SUITE_START (pldm_fwup_protocol_fd_commands);
 TEST (pldm_fwup_protocol_fd_commands_test_query_device_identifiers);
 TEST (pldm_fwup_protocol_fd_commands_test_get_firmware_parameters);
 TEST (pldm_fwup_protocol_fd_commands_test_request_update);
-TEST (pldm_fwup_protocol_fd_commands_test_update_component);
 TEST (pldm_fwup_protocol_fd_commands_test_pass_component_table);
+TEST (pldm_fwup_protocol_fd_commands_test_update_component);
 //TEST (pldm_fwup_protocol_fd_commands_test_get_package_data);
 //TEST (pldm_fwup_protocol_fd_commands_test_get_device_meta_data);
 
